45286aadd2253362bf412e2482cd5b3900bf0d10
Maylis <maylisdanjon@gmail.com>
2025-11-17 21:14:49 +0100

    Actualizo ej1b1.py

--- 1b/ej1b1.py
+++ 1b/ej1b1.py
@@ -16,28 +16,18 @@ Ejemplo:
     Salida:
     87
 
-Enunciat:
-Implementar la funció 'obtain_max(list_numbers)' que rep 
-com a paràmetre una llista no buida de nombres enters i retorna
-el número major de la llista.
-
-Recorda que en Python existeix la funció anomenada 'max'
-
-Paràmetres:
-- list_numbers: Llista de nombres enters
-
-Exemple:
-    Entrada:
-    obtain_max([1, 45, 87, 21, 0, 23, 28])
-
-    Sortida:
-    87
 """
 
 def obtain_max(list_numbers):
     # Write here your code
+
+    max = list_numbers[0]
+    
+    for i in list_numbers[1:]:
+        if  i > max:
+            max = i
+    
+    return max
     pass
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(obtain_max([1, 45, 87, 21, 0, 23, 28]))
+print(obtain_max([1, 45, 87, 21, 0, 23, 28]))
