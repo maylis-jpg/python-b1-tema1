ed526c2ca267e45c623f6c0b937c776121988ee2
Maylis <maylisdanjon@gmail.com>
2025-11-17 19:56:19 +0100

    Actualizo ej1a3.py

--- 1a/ej1a3.py
+++ 1a/ej1a3.py
@@ -14,27 +14,17 @@ Ejemplo:
     Salida:
     !dlrow olleH
 
-Enunciat:
-Implementa una funció anomenada "invert_text(text_chain)" que rebi com
-paràmetre una cadena de text de tipus string anomenada 'text_chain' i torni
-el text invertit.
-
-Paràmetres:
-- text_chain: Aquest paràmetre només admet strings.
-
-Exemple:
-     Entrada:
-     invert_text('Hello world!')
-
-     Sortida:
-     !dlrow olleH
-
 '''
 
 def invert_text(text_chain:str):
     # Write here your code
+
+    inv_text = ''
+    
+    for letra in text_chain[::-1]:
+        inv_text += letra
+    
+    return inv_text
     pass
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(invert_text("Hello world!"))
+print(invert_text("Hello world!"))
