fe95c306c290b0acbeeb0c7b47bb639aac9f1554
Maylis <maylisdanjon@gmail.com>
2025-11-22 13:14:24 +0100

    Actualizo ej1c4.py

--- 1c/ej1c4.py
+++ 1c/ej1c4.py
@@ -16,39 +16,26 @@ Ejemplo:
     Salida:
     True
 
-
-
-Enunciat:
-
-Enunciat:
-Escriu una funció anomenada is_palindrome(word) que rebi com a paràmetre
-una cadena word i verifiqui si és un palíndrom utilitzant recursió.
-La funció ha de tornar True si la cadena és un palíndrom i False a
-cas contrari.
-
-Paràmetres:
-     word (str): una cadena de caràcters.
-
-Exemple:
-     Entrada:
-     word = "racecar"
-     print(is_palindrome(word))
-
-     Sortida:
-     True
-
 """
 
 
 def is_palindrome(word):
     # Write here your code
+
+    middle = int(len(word)/2)
+
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+    
+    return is_palindrome(word[1:-1])
+
     pass
 
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# word = "level"
-# print(f"Is '{word}' word palindrome?", is_palindrome(word))
-#
-# word = "juan"
-# print(f"Is '{word}' word palindrome?", is_palindrome(word))
+word = "level"
+print(f"Is '{word}' word palindrome?", is_palindrome(word))
+
+word = "juan"
+print(f"Is '{word}' word palindrome?", is_palindrome(word))
