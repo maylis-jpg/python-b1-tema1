e3897b541086badf9b86188f789ed64ba424989c
Maylis <maylisdanjon@gmail.com>
2025-11-22 12:50:13 +0100

    Actualizo ej1c2.py

--- 1c/ej1c2.py
+++ 1c/ej1c2.py
@@ -15,32 +15,18 @@ Ejemplo
     Salida:
     [5, 4, 3, 2, 1]
 
-Enunciat:
-
-Escriu una funció anomenada 'invert_list(lst)' que rebi com a paràmetre
-una llista 'lst' i la inverteixi utilitzant recursió. La funció ha
-tornar la llista invertida.
-
-Paràmetres:
-     lst (list): una llista d'elements.
-
-Exemple
-     Entrada:
-     lst = [1, 2, 3, 4, 5]
-     print(invert_list(lst))
-
-     Sortida:
-     [5, 4, 3, 2, 1]
-
 """
 
 
 def invert_list(lst):
     # Write here your code
+    
+    if len(lst) <= 1:
+        return lst
+    return [lst[-1]] + invert_list(lst[:-1])
+    
     pass
 
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# lst = [1, 20, 3, 40, 5]
-#print(invert_list(lst))
+lst = [1, 20, 3, 40, 5]
+print(invert_list(lst))
