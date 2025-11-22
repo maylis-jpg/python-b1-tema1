609970fd0188af2124c49c346e6e483fb5ed1894
Maylis <maylisdanjon@gmail.com>
2025-11-22 11:10:01 +0100

    Actualizo ej1b3.py

--- 1b/ej1b3.py
+++ 1b/ej1b3.py
@@ -22,28 +22,6 @@ Los valores de los parámetros que recibe esta función son:
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 6, 8, 10]
 
-Enunciat:
-Mitjançant la llibreria 'matplotlib', implementa una funció anomenada
-'line_graph(x,y)', s'ha de realitzar únicament la configuració
-per a la gràfica (sense mostrar res en pantalla).
-D'aquesta forma, hauràs de configurar el títol, que serà 'Graph';
-els labels dels eixos, que seran 'Axis X' i 'Axis I' i activar
-el grid.
-
-Pots trobar la documentació de la llibreria 'matplotlib' en el
-següent enllaç: https://matplotlib.org/stable/users/index
-
-En concret, et recomanem que consultis:
-* https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.title.html
-* https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.xlabel.html
-* https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.ylabel.html
-* https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.grid.html
-
-Els valors dels paràmetres que rep aquesta funció són:
-    
-     x = [1, 2, 3, 4, 5]
-     y = [2, 4, 6, 8, 10]
-
 """
 
 import matplotlib.pyplot as plt
@@ -52,8 +30,14 @@ import matplotlib.pyplot as plt
 # Aquesta funció haurà de configurar la gràfica en la variable plt
 def line_graph(x, y):
     # Write here your code
+
+    plt.plot(x, y)
+
+    plt.xlabel('Axis X')
+    plt.ylabel('Axis Y')
+    plt.grid(axis='x', color='0.95')
+    plt.title('Graph')
+    
     pass
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# line_graph([1, 2, 3, 4, 5], [2, 4, 6, 8, 10])
+line_graph([1, 2, 3, 4, 5], [2, 4, 6, 8, 10])
