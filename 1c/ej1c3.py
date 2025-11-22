33ac7d7b988778183ee8f4b092a39d2dd1f7fa5a
Maylis <maylisdanjon@gmail.com>
2025-11-22 12:58:49 +0100

    Actualizo ej1c3.py

--- 1c/ej1c3.py
+++ 1c/ej1c3.py
@@ -16,33 +16,25 @@ Ejemplo:
     Salida:
     7
 
-
-Enunciat:
-
-Implementa una funció anomenada find_max(lst) que trobi el valor màxim en una
-llista de números utilitzant recursió. La funció ha de tornar el valor
-màxim de la llista.
-
-Paràmetres:
-     lst (List): llista de nombres enters o flotants
-
-Exemple:
-     Entrada:
-     numbers_list = [1, 5, 2, 7, 3]
-     fid_max(numbers_list)
-
-     Sortida:
-     7
-
 """
 
 
 def find_max(lst):
     # Write here your code
+
+    result = []
+
+    if len(lst) == 1:
+        return lst[0]
+    if lst[0] < find_max(lst[1:]):
+        result = find_max(lst[1:])
+    else:
+        result = lst[0]
+    
+    return result
+
+
     pass
 
-
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# numbers_list = [1, 5, 2, 7, 3]
-# print(find_max(numbers_list))
+numbers_list = [1, 5, 2, 7, 3]
+print(find_max(numbers_list))
